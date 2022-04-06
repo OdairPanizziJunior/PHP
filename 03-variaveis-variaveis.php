5c6c275af9b863ab7374c7b79d272d202691ac84
Odair Panizzi Júnior <101676143+OdairPanizziJunior@users.noreply.github.com>
2022-04-06 16:21:00 -0300

    Upload de Arquivos

---
+++ 03-variaveis-variaveis.php
@@ -0,0 +1,22 @@
+<?php
+// Variáveis Variaveis
+
+$bebida = "refrigerante";
+
+$$bebida = "Guaraná";
+
+echo $refrigerante;
+
+
+///////////////////
+echo "<br>";
+
+$destino = "cidade";
+
+$$destino = "Ilhéus";
+
+echo $cidade;
+
+////////////////////
+echo "<br>";
+
