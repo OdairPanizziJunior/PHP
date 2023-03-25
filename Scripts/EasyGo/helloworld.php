b37e066c01aca3c14eaf840728e5e7b356520433
OdairPanizziJunior <odairjunior36@gmail.com>
2023-03-24 21:00:13 -0300

    Update

---
+++ Scripts/EasyGo/helloworld.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF8"/>
+        <title>EasyGo</title>
+        <style>
+            h1 {
+                color:blue;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>PHP</h1>
+        <?php
+        $var = "Hi there!";
+        echo $var;
+        echo "<br>";
+        $n = "Odair";
+        echo "My name is $n";
+        echo "<br>";
+        echo date("d/m/y");
+        ?>
+    </body>
+</html>
